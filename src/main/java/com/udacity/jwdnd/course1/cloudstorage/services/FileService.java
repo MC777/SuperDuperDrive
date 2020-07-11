0c00d6950c33fc74a68df4effbf15c2717e02159
Marek Ciesla <ciesla.marek@gmail.com>
2020-07-11 23:25:21 +0200

    feat: add FileService class

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,49 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class FileService {
+    @Autowired
+    private FileMapper fileMapper;
+
+    public File uploadFile(MultipartFile multipartFile, Long userId) throws IOException {
+        File newFile = new File(
+                multipartFile.getOriginalFilename(),
+                multipartFile.getContentType(),
+                multipartFile.getSize(),
+                multipartFile.getBytes(),
+                userId
+        );
+
+        try {
+            fileMapper.save(newFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newFile;
+    }
+
+    public List<File> getAllFiles(Long userId) {
+        return fileMapper.findFilesByUserId(userId);
+    }
+
+    public File getFileById(Long fileId) throws IOException {
+        return fileMapper.findById(fileId);
+    }
+
+    public void deleteFile(Long fileId) throws IOException {
+        try {
+            fileMapper.deleteById(fileId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
