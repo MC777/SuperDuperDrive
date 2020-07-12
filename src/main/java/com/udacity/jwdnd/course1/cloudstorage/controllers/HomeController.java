3c5210afed94c9561234f7276f2c28af726078fe
Marek Ciesla <ciesla.marek@gmail.com>
2020-07-12 17:53:25 +0200

    feat: add HomeController to see home page view with files

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,14 +1,31 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-    @GetMapping
-    public String getLoginPage() {
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+    public String homepageView(Authentication authentication, Model model) {
+        String username = authentication.getName();
+        User user = userService.getUser(username);
+        model.addAttribute("files", fileService.getAllFiles(user.getUserid()));
         return "home";
     }
 }
