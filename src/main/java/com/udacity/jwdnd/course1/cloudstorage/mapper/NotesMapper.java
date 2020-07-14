146c72303bbfd6738c73ffc8e37f8471cbdb7d78
Marek Ciesla <ciesla.marek@gmail.com>
2020-07-14 23:59:44 +0200

    feat: update NoteMapper class

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
@@ -12,18 +12,18 @@ public interface NotesMapper {
     @Select("SELECT * FROM notes")
     List<Note> findAll();
 
-    @Select("SELECT * FROM notes WHERE userid = #{userId}")
+    @Select("SELECT * FROM notes WHERE userid = #{userid}")
     List<Note> findByUserId(Long userId);
 
-    @Select("SELECT * FROM notes WHERE noteid = #{id}")
-    Note findById(Long id);
+    @Select("SELECT * FROM notes WHERE userid = #{userid} AND noteid = #{noteid} ")
+    public Note findById(Long noteid, Long userid);
 
-    @Insert("INSERT INTO notes (notetitle, notedescription, userid) VALUES (#{note.noteTitle}, #{note.noteDescription}, #{userId})")
-    Integer create(@Param("note") Note note, Long userId);
+    @Insert("INSERT INTO notes (notetitle, notedescription, userid) VALUES (#{note.noteTitle}, #{note.noteDescription}, #{userid})")
+    Integer create(@Param("note") Note note, Long userid);
 
-    @Update("UPDATE notes SET notetitle = #{note.noteTitle}, notedescription = #{note.noteDescription} WHERE noteid = #{note.noteId} AND userid = #{userId}")
-    Integer update(@Param("note") Note note, Long userId);
+    @Update("UPDATE notes SET notetitle = #{note.noteTitle}, notedescription = #{note.noteDescription} WHERE noteid = #{note.noteId} AND userid = #{userid}")
+    Integer update(@Param("note") Note note, Long userid);
 
-    @Delete("DELETE FROM notes WHERE noteid = #{id} AND userid = #{userId}")
-    Integer delete(Long id, Long userId);
+    @Delete("DELETE FROM notes WHERE noteid = #{id} AND userid = #{userid}")
+    Integer delete(Long id, Long userid);
 }
