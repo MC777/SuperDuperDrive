5d42133065987c48c57069de84a4bae8ef6fb2fb
Marek Ciesla <ciesla.marek@gmail.com>
2020-07-20 22:46:11 +0200

    tests: signup and login test

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,12 +2,14 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
@@ -33,9 +35,21 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
-	public void getLoginPage() {
+	@Order(1)
+	public void getLoginPage() throws InterruptedException{
+		driver.get("http://localhost:" + this.port + "/signup");
+		driver.findElement(By.id("inputFirstName")).sendKeys("Tom");
+		driver.findElement(By.id("inputLastName")).sendKeys("Hanks");
+		driver.findElement(By.id("inputUsername")).sendKeys("TomH");
+		driver.findElement(By.id("inputPassword")).sendKeys("TomPass");
+		Thread.sleep(5000);
+		driver.findElement(By.id("submit-button")).click();
 		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+		driver.findElement(By.id("inputUsername")).sendKeys("TomH");
+		driver.findElement(By.id("inputPassword")).sendKeys("TomPass");
+		driver.findElement(By.id("submit-button")).click();
+		Thread.sleep(5000);
+		Assertions.assertEquals("Home", driver.getTitle());
 	}
 
 }
